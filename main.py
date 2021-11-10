41a0c80c7eda280a482f4a7230024369b9b45577
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-11 00:57:06 +0200

    feat: add initial pdf generation

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+from fpdf import FPDF
+import click
 
-def main():
+class Report:
     pass
+
+class ReportStyle:
+    pass
+
+class ReportGenerator:
+    pass
+
+@click.command()
+@click.argument("output")
+def main(output: str):
+    pdf = FPDF("portrait", "mm", "A4")
+    pdf.add_page()
+    pdf.set_font("times")
+    pdf.cell(40, 10, "Hello, World!")
+    pdf.output(output)
 
 if __name__ == "__main__":
     main()
