676d551f4575d21683a28e7e6170def2b314d848
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-12 13:24:43 +0200

    feat: use pydantic for input validation

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from fpdf import FPDF
 from PIL import Image
 from enum import Enum
-from dataclasses import dataclass, field
 from fpdf.fpdf import ToCPlaceholder
 from fpdf.outline import OutlineSection
 from typing import Optional
@@ -10,25 +9,25 @@ import inspect
 import click
 import sys
 from datetime import date
+from pydantic.error_wrappers import ValidationError
 import toml
+from pydantic import BaseModel, Field
 
-class Gender(Enum):
-    MALE = 1
-    FEMALE = 2
+class Gender(str, Enum):
+    MALE = "male"
+    FEMALE = "female"
 
-@dataclass
-class ReportSection:
+class ReportSection(BaseModel):
     title: str
 
-    problem: Optional[str] = None
-    project_location: Optional[str] = None
-    professors_notes: Optional[str] = None
+    problem: Optional[str]
+    project_location: Optional[str]
+    professors_notes: Optional[str]
 
     def __str__(self) -> str:
         return f"ReportSection[{self.title}]"
 
-@dataclass
-class Report:
+class Report(BaseModel):
     title: str
 
     student_name: str
@@ -37,13 +36,13 @@ class Report:
     professor_name: str
     professor_gender: Gender
 
-    sections: list[ReportSection] = field(default_factory=list)
+    sections: list[ReportSection] = Field(default_factory=list)
 
-    university_name: str = field(default="Kauno technologijos universitetas")
-    faculty_name: str = field(default="Informatikos fakultetas")
-    sub_title: str = field(default="Laboratorinių darbų ataskaita")
-    city_name: str = field(default="Kaunas")
-    year: int = field(default=date.today().year)
+    university_name: str = Field(default="Kauno technologijos universitetas")
+    faculty_name: str = Field(default="Informatikos fakultetas")
+    sub_title: str = Field(default="Laboratorinių darbų ataskaita")
+    city_name: str = Field(default="Kaunas")
+    year: int = Field(default=date.today().year)
 
     def __str__(self) -> str:
         return f"Report[{self.student_name}, {self.title}]"
@@ -242,27 +241,7 @@ def from_dict_to_dataclass(cls, data):
 
 def read_report_from_toml(filename: str) -> Report:
     parsed_toml = toml.load(filename)
-
-    if isinstance(parsed_toml["student_gender"], str):
-        gender = parsed_toml["student_gender"].upper()
-        try:
-            parsed_toml["student_gender"] = Gender[gender]
-        except KeyError:
-            pass
-
-    if isinstance(parsed_toml["professor_gender"], str):
-        gender = parsed_toml["professor_gender"].upper()
-        try:
-            parsed_toml["professor_gender"] = Gender[gender]
-        except KeyError:
-            pass
-
-    if isinstance(parsed_toml["sections"], list):
-        sections = parsed_toml["sections"]
-        for i in range(len(sections)):
-            sections[i] = from_dict_to_dataclass(ReportSection, sections[i])
-
-    return from_dict_to_dataclass(Report, parsed_toml)
+    return Report(**parsed_toml)
 
 @click.command()
 @click.argument("input", type=click.Path(exists=True, readable=True, dir_okay=False))
@@ -271,8 +250,8 @@ def main(input: str, output: str):
     report = None
     try:
         report = read_report_from_toml(input)
-    except TypeError as e:
-        click.echo(click.style(f"Field error in input file ({input}):", fg="red"))
+    except ValidationError as e:
+        click.echo(click.style(f"Validation error from input file ({input}):", fg="red"))
         click.echo(click.style(e, fg="red"))
         sys.exit(1)
     except toml.TomlDecodeError as e:
