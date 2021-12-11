525f979478efad65aac65397b6466b78914f6d21
Rokas Puzonas <rokas.puz@gmail.com>
2021-12-11 13:48:01 +0200

    feat: add GNU GPL v3 license

--- ooppdfgen/report.py
+++ ooppdfgen/report.py
@@ -1,3 +1,22 @@
+"""
+    Copyright (C) 2021 - Rokas Puzonas <rokas.puz@gmail.com>
+
+    This file is part of KTU OOP Report Generator.
+
+    KTU OOP Report Generator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KTU OOP Report Generator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KTU OOP Report Generator. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from dataclasses import dataclass, field
 from typing import Optional, Union
 from enum import Enum
